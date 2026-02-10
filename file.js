ca33ff663085bed99a3c217ec8cff5f9ea0f832f
Mohammad Asadullah <87116101+Asadullah2412@users.noreply.github.com>
2026-02-10 20:45:58 +0530

    d

--- file.js
+++ file.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
+const os = require("os")
 
+console.log(os.cpus().length)
 
 // fs.writeFileSync("./text.txt", "my first node file");
 
@@ -16,4 +18,21 @@ const fs = require("fs");
 // })
 
 
-fs.appendFileSync('./text.txt', new Date().getDate().toLocaleString());
+// fs.appendFileSync('./text.txt', new Date().getDate().toLocaleString());
+
+// blocking
+// console.log('1')
+// const result = fs.readFileSync('./text.txt', 'utf-8');
+// console.log(result);
+
+// console.log('2')
+
+
+// NON blocking
+
+console.log('1')
+fs.readFile('text.txt', 'utf-8', (err, result) => {
+    console.log(result)
+})
+
+console.log('2')
