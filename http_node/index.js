4fbf11777f2438d3c5631a29de13a2dc7c8dc057
Mohammad Asadullah <87116101+Asadullah2412@users.noreply.github.com>
2026-02-11 20:19:12 +0530

    added express

--- http_node/index.js
+++ http_node/index.js
@@ -5,19 +5,24 @@ const { defaultMaxListeners } = require('events');
 
 const myServer = http.createServer((req, res) => {
     if (req.url === "/favicon.ico") return res.end();
-    // console.log(req);
-    // const log = `${Date.now()} : ${req.url} new Req Received\n`;
-    const log = `${new Date().toISOString()} :${req.url} new Req Received\n`;
+
+    const log = `${new Date().toISOString()} : ${req.method} ${req.url} new Req Received\n`;
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl)
     fs.appendFile('log.txt', log, (err, data) => {
         // res.end("Hello from Server Again")
-        switch (req.url) {
+        switch (myUrl.pathname) {
             case '/':
                 res.end("Welcome to Homepage");
                 break;
 
             case "/about":
-                res.end("I am Http Server on port 8000");
+                const username = myUrl.query.myname
+                res.end(`HI , ${username}`);
                 break;
+            case "/search":
+                const search = myUrl.query.search_query;
+                res.end("here are the results for " + search);
             default:
                 res.end("404 not found bro 🧐")
 
