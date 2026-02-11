36a944a27e985207db6e1984c52e2b2e55606772
Mohammad Asadullah <87116101+Asadullah2412@users.noreply.github.com>
2026-02-11 20:33:13 +0530

    created server with express

--- http_node/index.js
+++ http_node/index.js
@@ -1,34 +1,57 @@
 const http = require('http');
-const fs = require("fs");
-const url = require("url")
+// const fs = require("fs");
+// const url = require("url")
+const express = require('express')
+
 const { defaultMaxListeners } = require('events');
+const app = express();
 
-const myServer = http.createServer((req, res) => {
-    if (req.url === "/favicon.ico") return res.end();
-
-    const log = `${new Date().toISOString()} : ${req.method} ${req.url} new Req Received\n`;
-    const myUrl = url.parse(req.url, true);
-    console.log(myUrl)
-    fs.appendFile('log.txt', log, (err, data) => {
-        // res.end("Hello from Server Again")
-        switch (myUrl.pathname) {
-            case '/':
-                res.end("Welcome to Homepage");
-                break;
-
-            case "/about":
-                const username = myUrl.query.myname
-                res.end(`HI , ${username}`);
-                break;
-            case "/search":
-                const search = myUrl.query.search_query;
-                res.end("here are the results for " + search);
-            default:
-                res.end("404 not found bro 🧐")
-
-        }
-
-    })
+app.get("/", (req, res) => {
+    return res.send("Hello from About Page");
 });
 
-myServer.listen(8000, () => console.log("Server started!"))
+app.get("/about", (req, res) => {
+    // return res.send("Hello from About Page" + ' Hey ' + req.query.name);
+    return res.send(`hello ${req.query.name}`)
+});
+
+app.listen(8000, () => console.log("server Started 👿"))
+// const myServer = http.createServer(app)
+
+
+// myServer.listen(8000, () => console.log("Server started!"))
+
+
+
+
+
+
+
+
+// const myServer = http.createServer((req, res) => {
+//     if (req.url === "/favicon.ico") return res.end();
+
+//     const log = `${new Date().toISOString()} : ${req.method} ${req.url} new Req Received\n`;
+//     const myUrl = url.parse(req.url, true);
+//     console.log(myUrl)
+//     fs.appendFile('log.txt', log, (err, data) => {
+//         // res.end("Hello from Server Again")
+//         switch (myUrl.pathname) {
+//             case '/':
+//                 res.end("Welcome to Homepage");
+//                 break;
+
+//             case "/about":
+//                 const username = myUrl.query.myname
+//                 res.end(`HI , ${username}`);
+//                 break;
+//             case "/search":
+//                 const search = myUrl.query.search_query;
+//                 res.end("here are the results for " + search);
+//             default:
+//                 res.end("404 not found bro 🧐")
+
+//         }
+
+//     })
+// });
