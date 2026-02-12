0d29c3ab164a6622a104bcdfe644de1219f86307
Mohammad Asadullah <87116101+Asadullah2412@users.noreply.github.com>
2026-02-12 21:19:02 +0530

    pending

---
+++ project_01/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const users = require("./MOCK_DATA.json");
+const fs = require('fs')
+const app = express();
+const PORT = 8000;
+
+app.use(express.urlencoded({ extended: false }))
+
+// Routes
+app.get('/api/users', (req, res) => {
+    return res.json(users);
+});
+
+app.get('/users', (req, res) => {
+    const html = `
+<ul>
+${users.map((user) => `<li>${user.first_name}</li>`)}
+</ul>
+`;
+    res.send(html)
+})
+
+app.route("/api/users/:id").get((req, res) => {
+
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+}).put((req, res) => {
+    // Edit user with id
+    res.send('pending');
+}).delete((req, res) => {
+    // delete user with id
+    res.send('pending');
+})
+
+
+app.post('/api/users', (req, res) => {
+    const body = req.body;
+    // console.log('Body', body)
+    users.push({ ...body, id: users.length + 1 });
+    fs.writeFile('./MOCK_DATA.json', JSON.stringify(users), (err, data) => {
+        return res.json({ status: "pending" });
+    })
+    return res.json({ status: "Success", id: users.length });
+
+});
+
+
+
+
+//  start server
+app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`))
